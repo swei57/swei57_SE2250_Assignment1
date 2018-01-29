d43d45c6a918dab17972a3e543b6e037309bee40
swei57 <swei57@users.noreply.github.com>
2018-01-29 09:58:13 -0500

    Added project to github

---
+++ Assignment1_Unity/Assets/_scripts/ResetPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+    Vector3 initialPosition;
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+   
+  }
+    void OnMouseOver()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (Input.GetMouseButtonDown(1)) {
+                transform.position = initialPosition;
+            }
+        }
+    }
+}
---
+++ Assignment1_Unity/Assets/_shapes/ChangeBoxAngle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBoxAngle : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+       // transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void OnMouseOver() {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //rotate the box in random coordinates when click on by the mouse
+                transform.eulerAngles = new Vector3(Random.Range(180.0f, 188.0f), Random.Range(0.0f, 360.0f), Random.Range(180.0f, 188.0f));
+            }
+        }
+    }
+}
